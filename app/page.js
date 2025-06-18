25b16082cb7286e53fe5d4bf05721fddb131b5e8
Keerti2005 <keertimadhuvantika.a2022ece@sece.ac.in>
2025-06-18 18:21:08 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
-import { personalData } from "@/utils/data/personal-data";
+// app/page.tsx or pages/index.tsx (depending on file structure)
+
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
@@ -8,18 +9,7 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 import Achievements from "./components/homepage/achievement/achievement-section";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
- 
-}
-
-export default async function Home() {
-
+export default function Home() {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <HeroSection />
